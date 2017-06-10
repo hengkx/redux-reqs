791a1e372c8553b26668abb11f82fcfb04e6e790
hengkx <ycxzhkx@gmail.com>
2017-06-11 00:37:31 +0800

    reucer default return everyone same

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,6 @@
+# 0.1.1(2017-06-11)
+### Bug Fixs
+* reucer default return everyone same
+
 # 0.1.0(2017-06-11)
 * simple create async request
--- src/index.js
+++ src/index.js
@@ -6,75 +6,74 @@ import camelCase from 'lodash.camelcase';
 import pathToRegexp from 'path-to-regexp';
 
 export default (actions, opts = {}) => {
-    const resultSufix = opts.resultSufix || '_RESULT';
-    function getReducers() {
-        const reducers = {};
-        actions.forEach(item => {
-            const { type } = item;
-            reducers[type] = (state) => ({
-                ...state,
-                isfetching: true
-            });
-            reducers[`${type}${resultSufix}`] = (state, action) => ({
-                ...state,
-                isfetching: false,
-                updateResult: action.payload
-            });
-        });
-        return reducers;
+  const resultSufix = opts.resultSufix || '_RESULT';
+  function getReducers() {
+    const reducers = {};
+    actions.forEach(item => {
+      const { type } = item;
+      reducers[type] = (state) => ({
+        ...state,
+        isfetching: true
+      });
+      reducers[`${type}${resultSufix}`] = (state, action) => ({
+        ...state,
+        isfetching: false,
+        [camelCase(`${type}${resultSufix}`)]: action.payload
+      });
+    });
+    return reducers;
+  }
+
+  function* request(data) {
+    const { type, payload, meta } = data;
+    let url = meta.url;
+    const actionResult = createAction(`${type}_RESULT`);
+    try {
+      yield put(beginTask());
+
+      if (meta.method === 'put') {
+        const toPath = pathToRegexp.compile(url);
+        url = toPath(payload);
+      }
+
+      const res = yield call(axios, url, { method: meta.method, data: payload });
+
+      yield put(actionResult(res));
+    } catch (error) {
+      yield put(actionResult(error));
+    } finally {
+      yield put(endTask());
     }
+  }
+  function metaCreator(url, method = 'get') {
+    return (_, meta) => ({
+      url,
+      method,
+      ...meta
+    });
+  }
+  function getCreateActions() {
+    const result = {};
+    actions.forEach(action => {
+      result[camelCase(action.type)] = createAction(action.type, null,
+        metaCreator(action.url, action.method));
+    });
+    return result;
+  }
 
-    function* request(data) {
-        console.log(data);
-        const { type, payload, meta } = data;
-        let url = meta.url;
-        const actionResult = createAction(`${type}_RESULT`);
-        try {
-            yield put(beginTask());
+  const actionCreators = getCreateActions();
 
-            if (meta.method === 'put') {
-                const toPath = pathToRegexp.compile(url);
-                url = toPath(payload);
-            }
+  function getWatchSagas() {
+    const result = [];
+    actions.forEach(action => {
+      result.push(takeEvery(action.type, request));
+    });
+    return result;
+  }
 
-            const res = yield call(axios, url, { method: meta.method, data: payload });
-
-            yield put(actionResult(res));
-        } catch (error) {
-            yield put(actionResult(error));
-        } finally {
-            yield put(endTask());
-        }
-    }
-    function metaCreator(url, method = 'get') {
-        return (_, meta) => ({
-            url,
-            method,
-            ...meta
-        });
-    }
-    function getCreateActions() {
-        const result = {};
-        actions.forEach(action => {
-            result[camelCase(action.type)] = createAction(action.type, null,
-                metaCreator(action.url, action.method));
-        });
-        return result;
-    }
-
-    const actionCreators = getCreateActions();
-
-    function getWatchSagas() {
-        const result = [];
-        actions.forEach(action => {
-            result.push(takeEvery(action.type, request));
-        });
-        return result;
-    }
-
-    return {
-        actionCreators,
-        handleActions: handleActions(getReducers(), opts.defaultState || {}),
-        watchSagas: getWatchSagas()
-    };
+  return {
+    actionCreators,
+    handleActions: handleActions(getReducers(), opts.defaultState || {}),
+    watchSagas: getWatchSagas()
+  };
 };
